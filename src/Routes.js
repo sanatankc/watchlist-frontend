c903efff6059907f0f7513ff0d6ac2e9dc33aa9d
sanatankumar <sanatan.iit@gmail.com>
2018-05-26 08:20:59 +0530

    added UI for top controls in root

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import Login from './page/login'
-import App from './App'
+import RootPage from './page/root'
 
 const Routes = () => (
   <Switch>
-    <PrivateRoute exact path='/' component={App} />
+    <PrivateRoute exact path='/' component={RootPage} />
     <Route exact path='/login' component={Login} />
     <Route path='/' render={props => 'Not Found' } />
   </Switch>
