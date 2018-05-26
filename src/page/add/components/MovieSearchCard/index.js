5d9fb6a6203753bb7d2fa5bb2e87743956bf60c3
sanatankumar <sanatan.iit@gmail.com>
2018-05-26 14:44:16 +0530

    scaled card smaller for better desgin

--- src/page/add/components/MovieSearchCard/index.js
+++ src/page/add/components/MovieSearchCard/index.js
@@ -5,33 +5,35 @@ import { boxShadow, themeColor } from '../../../../constants'
 const Card = styled.div`
   background: white;
   box-shadow: ${boxShadow};
-  width: 340px;
+  width: 270px;
   border-radius: 4px;
   overflow: hidden;
 `
 const PostersWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 335px;
+  height: 250px;
 `
 const BackgroundPoster = styled.div`
   position: absolute;
-  width: 200px;
-  height: 300px;
+  width: 150px;
+  height: 225px;
   top: 0;
   left: 0;
   background: url("https://image.tmdb.org/t/p/w200/hYtmqHbCuemuA6ICUylWRfP7bR6.jpg");
+  background-size: cover;
   filter: url(#bluish);
 `
 
 const FrontPoster = styled.div`
   position: absolute;
-  width: 200px;
-  height: 300px;
+  width: 150px;
+  height: 225px;
   bottom: 0;
   right: 0;
   box-shadow: -5px 0px 25px 0 rgba(46, 61, 73, 0.4);
   background: url("https://image.tmdb.org/t/p/w200/hYtmqHbCuemuA6ICUylWRfP7bR6.jpg");
+  background-size: cover;
 `
 const TitleWrapper = styled.div`
   width: 100%;
@@ -40,7 +42,7 @@ const TitleWrapper = styled.div`
 `
 
 const Title = styled.div`
-  font-size: 18px;
+  font-size: 16px;
   letter-spacing: 2.2px;
   margin-top: 30px;
   width: 100%;
@@ -54,7 +56,7 @@ const WatchListBtn = styled.button`
   padding: 0;
   margin: 0;
   width: 100%;
-  font-size: 18px;
+  font-size: 16px;
   padding: 10px 0;
   background: ${themeColor};
   color: white;
@@ -70,7 +72,7 @@ const MovieSearchCard = () => (
       <FrontPoster />
     </PostersWrapper>
     <TitleWrapper>
-      <Title>A Day in the Family but let's emulate fake long title</Title>
+      <Title>A Death in the Gunj</Title>
     </TitleWrapper>
     <WatchListBtn>+ Add to Watchlist</WatchListBtn>
   </Card>
