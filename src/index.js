dec6d254c442d12076d743d75756586e5c5e5611
sanatankumar <sanatan.iit@gmail.com>
2018-05-28 09:17:23 +0530

    fixed login bug

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from "react-apollo"
-import ApolloClient from "apollo-boost"
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 import './index.css'
 import Routes from './Routes'
 import NavBar from './components/NavBar'
 import registerServiceWorker from './registerServiceWorker'
 
+const httpLink = createHttpLink({
+  uri: 'https://watchlist-ctzcjyimlo.now.sh/graphql',
+  credentials: 'include'
+})
+
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('access_token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
 const client = new ApolloClient({
-  uri: 'https://watchlist-cnctfkofik.now.sh/graphql'
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 })
 
 ReactDOM.render(
--- src/page/add/components/MovieSearchCard/index.js
+++ src/page/add/components/MovieSearchCard/index.js
@@ -1,144 +1,24 @@
 import React, { Component, Fragment } from 'react'
-import styled, { keyframes } from 'styled-components'
-import { boxShadow, themeColor } from '../../../../constants'
-import placeHolder from './placeholder.png'
-import { Mutation } from 'react-apollo'
+import { Mutation, withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
-
-
-const blink = keyframes`
-  0% {
-    opacity: .2;
-  }
-  20% {
-    opacity: 1;
-  }
-  100% {
-    opacity: .2;
-  }
-`
-
-const Card = styled.div`
-  background: white;
-  box-shadow: ${boxShadow};
-  width: 270px;
-  border-radius: 4px;
-  overflow: hidden;
-  margin-top: 40px;
-  margin-left: 30px;
-`
-const PostersWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  height: 250px;
-`
-const BackgroundPosterContainer = styled.div`
-  position: absolute;
-  width: 150px;
-  height: 225px;
-  top: 0;
-  left: 0;
-`
-const BackgroundPoster = styled.div`
-  z-index: 2;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: url("${props => props.image || placeHolder}");
-  background-size: cover;
-  filter: url(#bluish);
-`
-
-const FrontPosterContainer = styled.div`
-  position: absolute;
-  width: 150px;
-  height: 225px;
-  bottom: 0;
-  right: 0;
-  box-shadow: -5px 0px 25px 0 rgba(46, 61, 73, 0.4);
-`
-const FrontPoster = styled.div`
-  z-index: 2;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: url("${props => props.image || placeHolder}");
-  background-size: cover;
-`
-
-const PlaceHolderImage = styled.div`
-  z-index: 1;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: url("${placeHolder}");
-  background-size: cover;
-  ${props => props.filtered && 'filter: url(#bluish);'}
-`
-
-const TitleWrapper = styled.div`
-  width: 100%;
-  padding: 0 10px;
-  box-sizing: border-box;
-`
-
-const Title = styled.div`
-  font-size: 16px;
-  letter-spacing: 2.2px;
-  margin-top: 30px;
-  height: 18px;
-  width: 100%;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`
-const WatchListBtn = styled.button`
-  border: none;
-  outline: none;
-  padding: 0;
-  margin: 0;
-  width: 100%;
-  font-size: 16px;
-  padding: 10px 0;
-  background: ${props => {
-    if (props.btnStatus === 'NORMAL') return themeColor
-    if (props.btnStatus === 'LOADING') return '#2EC4B691'
-    if (props.btnStatus === 'SUCCESS') return '#2EC4B6'
-  }};
-  cursor: ${props => {
-    if (props.btnStatus === 'NORMAL') return 'pointer'
-    if (props.btnStatus === 'LOADING') return 'not-allowed'
-    if (props.btnStatus === 'SUCCESS') return 'pointer'
-  }};
-  letter-spacing: 2.2px;
-  margin-top: 20px;
-  color: #fff;
-  transition: 0.3s all ease-in-out;
-  span {
-    animation-name: ${blink};
-    animation-duration: 1.4s;
-    animation-iteration-count: infinite;
-    animation-fill-mode: both;
-    &:nth-child(2) {
-      animation-delay: 0.2s;
-    }
-    &:nth-child(3) {
-      animation-delay: 0.4s;
-    }
-  }
-`
+import {
+  Card,
+  PostersWrapper,
+  BackgroundPosterContainer,
+  BackgroundPoster,
+  FrontPosterContainer,
+  FrontPoster,
+  PlaceHolderImage,
+  TitleWrapper,
+  Title,
+  WatchListBtn
+} from './index.style'
 
 const ADD_WATCHLIST = gql`
   mutation addMovie($tmdbId: String!, $movieName: String!) {
-    addMovie(: $tmdbId, movieName: $movieName) {
+    addMovie(tmdbId: $tmdbId, movieName: $movieName) {
       tmdbId
-      movieName
+
     }
   }`
 
@@ -163,6 +43,18 @@ class MovieSearchCard extends Component {
     }
   }
 
+  saveInWatchlist(addMovie) {
+    const { tmdbId, movieName } = this.props
+    console.log(tmdbId, movieName)
+    addMovie({
+      variables: {
+        tmdbId,
+        movieName
+      }
+    })
+  }
+
+
   render() {
     return (
       <Card>
@@ -179,14 +71,18 @@ class MovieSearchCard extends Component {
         <TitleWrapper>
           <Title>{this.props.movieName}</Title>
         </TitleWrapper>
-        {/* <Mutation mutation={ADD_WATCHLIST}> */}
-          <WatchListBtn btnStatus={this.state.btnStatus}>
-            {this.renderButtonContent()}
-          </WatchListBtn>
-        {/* </Mutation> */}
+
+        <Mutation mutation={ADD_WATCHLIST}>
+          {(addMovie, { data }) => (
+            <WatchListBtn btnStatus={this.state.btnStatus} onClick={() => {
+              this.saveInWatchlist(addMovie)
+            }}>
+              {this.renderButtonContent()}
+            </WatchListBtn>
+          )}
+        </Mutation>
       </Card>
     )
   }
 }
-
-export default MovieSearchCard
+export default withApollo(MovieSearchCard)
