73777f94003313c5991582113a8b112aad0b7dfe
sanatankumar <sanatan.iit@gmail.com>
2018-05-31 06:50:29 +0530

    changed fixed backend link

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import NavBar from './components/NavBar'
 import registerServiceWorker from './registerServiceWorker'
 
 const httpLink = createHttpLink({
-  uri: 'https://watchlist-jjdajaujvv.now.sh/graphql',
+  uri: 'https://watchlist-aacvtxpxrw.now.sh/graphql',
   credentials: 'include'
 })
 
